d885ab68ae9a7a59a984105dd4b82ecf2982c8f7
Vlad.Melnychenko <vlad.melnichenko@gmail.com>
2015-09-13 14:41:05 +0300

    2

--- get-lng.php
+++ get-lng.php
@@ -1,10 +1,33 @@
 <?php
+/**
+ * ajax-lng.
+ * PHP Version 5
+ * @package ajax-lng
+ * @link https://github.com/vladyslavml/ajax-lng
+ * @author Vladyslav Melnychecko  <vlad.melnychenko@outlook.com>
+ * @copyright 2015
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ * @note This program is distributed in the hope that it will be useful - WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+
+/**
+ * If session not defined  call session_start();
+ */
 if(!isset($_SESSION))
 {
     session_start();
 }
 
 
+/**
+ * Initial lng file
+ * GET request /get-lng.php?name=en-en
+ * return JSON language file
+ * @return string
+ */
 if (isset($_GET['name'])) {
       $file = file_get_contents(__DIR__.'../lng/'.$_GET['name'].'.json');
       $_SESSION["lng"]=$_GET['name'];
@@ -13,6 +36,12 @@ if (isset($_GET['name'])) {
       exit;
 }
 
+
+/**
+ * Return translation text for $name use in php scripts
+ * @param string $name
+ * @return string
+ */
 function tr($name) {
     $file = file_get_contents(__DIR__.'../lng/'.$_SESSION["lng"].'.json');
     $oLng = json_decode($file);
